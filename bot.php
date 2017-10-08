b2e83cab357c046b99b96976cf783c1d152d9b6a
aegiser <aegiser@hotmail.com>
2017-10-08 23:03:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,6 +13,31 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		if ($event['type'] == 'group'){
 			$groupId = $event['groupId'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => 'group chat'
+			];
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/push';
+			$data = [
+				'to' => Ub0c1ec272ac44dbb11f1defdcfed1f2e,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";			
 		}
 	}
 	foreach ($events['events'] as $event) {
