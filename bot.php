3e3bf2acc65ac6c43d2d15514563e900e31d93cb
aegiser <aegiser@hotmail.com>
2017-10-08 23:33:31 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,41 +9,13 @@ $events = json_decode($content, true);
 $groupId = 0;
 $userId = 0;
 if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-			$groupId = $event['groupId'];
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $event['type']
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-				'to' => Ub0c1ec272ac44dbb11f1defdcfed1f2e,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";			
-
-	}
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$groupId = $event['source']['group'];
+			 
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
