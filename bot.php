b13ab232d40a4ed1cd7c374d4a88015448af7d36
aegiser <aegiser@hotmail.com>
2017-10-08 23:35:19 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,7 +14,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$groupId = $event['source']['group'];
+			$groupId = $event['source']['groupId'];
 			 
 			// Get replyToken
 			$replyToken = $event['replyToken'];
