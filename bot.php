128d1cb70fbb878e221f54fd797bb22826deafb9
aegiser <aegiser@hotmail.com>
2017-10-13 14:01:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			if ($text == 'ShowId'){
+			if ($text == 'Id?'){
 
 				// Get replyToken
 				$replyToken = $event['replyToken'];
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => '[GroupId] '.$groupId. "\r\n" .'[UserId] '.$userId. "\r\n" .'[ReplyToken] '.$replyToken. "\r\n" .'[Text] '.$text
+					'text' => '[GroupId] '.$groupId. "\r\n" .'[UserId] '.$userId. "\r\n" .'[ReplyToken] '.$replyToken. "\r\n"
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
